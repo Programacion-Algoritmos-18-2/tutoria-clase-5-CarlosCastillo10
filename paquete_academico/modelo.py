04112e9edbe617a19bda2935737bbcf6bab2ec25
Carlos Castillo <carloscastillo090916@gmail.com>
2018-11-13 18:08:31 -0500

    Agregando ejercicio tutoria 5

--- paquete_academico/modelo.py
+++ paquete_academico/modelo.py
@@ -77,26 +77,45 @@ class EstPresencial(Estudiante):#Clase hija que era de la clase padre ("Estudian
         self.ciclo = c
 
     def agregar_num_creditos(self, creditos):  
-        self.agregar_num_creditos = creditos
+        self.num_creditos = creditos
 
         #Metodos de obtener 
-    def obtener_sueldo_fijo(self):
-        return self.sueldo_fijo
 
     def obtener_ciclo(self):
         return self.ciclo
 
     def obtener_num_creditos(self):
-        return self.agregar_num_creditos
+        return self.num_creditos
 
     
     def presentar_datos(self):
         #Llamamos al metodo presentar datos de la clase padre
-        cadena = "\nReporte\n\n%s %s-%d\nCiclo: %s\nCreditos: %d\nPertenece a ciudad %s, con una poblacion de %d\n" %(self.obtener_nombre(),self.obtener_apellido()
-            ,self.obtener_id_estudiante(),self.obtener_ciclo(),self.obtener_num_creditos(),self.obtener_ciudad().obtener_nombre(),self.obtener_ciudad().obtener_poblacion())
+        cadena = "\nReporte\n\n%s %s-%d\nCiclo: %s\nCreditos: %d\nPertenece a la ciudad de %s, con una poblacion de %d\n" %(self.obtener_nombre()
+            ,self.obtener_apellido(),self.obtener_id_estudiante(),self.obtener_ciclo(),self.obtener_num_creditos()
+            ,self.obtener_ciudad().obtener_nombre(),self.obtener_ciudad().obtener_poblacion())
 
         return cadena
 
+class EstDistancia(Estudiante):
+
+    def __init__(self):
+        
+        super(EstDistancia, self).__init__()       
+        self.modulo = 0
+        self.num_materias = 0
+    
+    def agregar_modulo(self, m):  
+        self.modulo = m
+
+    def agregar_num_materis(self, materias):  
+        self.num_materias = materias
+
+        #Metodos de obtener 
+    def obtener_modulo(self):
+        return self.modulo
+
+    def obtener_num_materias(self):
+        return self.num_materias
 
     
 
